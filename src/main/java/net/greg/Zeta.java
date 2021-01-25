79a8845ee56454e9f4e3a014ab8bd53ac2536267
gtrester <gtrester@USA-MAC-NIS1573.local>
2021-01-25 15:32:47 -0600

    any

---
+++ src/main/java/net/greg/Zeta.java
@@ -0,0 +1,12 @@
+package net.greg;
+
+public class Zeta {
+
+	public static void main(String ... any) {
+    System.err.println("\n\n\u001B[32mOk\u001B[0m\n\n");
+  }
+
+	public static final String OK = "\u001B[32m";
+  public static final String RED = "\u001B[31m";
+  public static final String NC = "\u001B[0m";
+}
