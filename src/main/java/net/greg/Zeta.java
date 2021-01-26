f703d8d51a7f158c265fbd6438ea3f7a52294dd7
gtrester <gtrester@USA-MAC-NIS1573.local>
2021-01-26 14:09:23 -0600

    1

--- src/main/java/net/greg/Zeta.java
+++ src/main/java/net/greg/Zeta.java
@@ -1,9 +1,17 @@
 package net.greg;
 
+import java.text.*;
+import java.time.*;
+import java.util.*;
+
 public class Zeta {
 
 	public static void main(String ... any) {
     System.err.println("\n\n\u001B[32mOk\u001B[0m\n\n");
+
+
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		System.out.println("Hello " + formatter.format(new Date()));
   }
 
 	public static final String OK = "\u001B[32m";
